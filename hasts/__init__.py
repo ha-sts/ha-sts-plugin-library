0b563fdc3c5e8e6fc0cc195208d295d3e8dac4a3
Daniel Williams <dwilliams@port8080.net>
2022-03-11 18:28:03 -0700

    Adding the step to upload the package to the GitHub Release.

--- hasts/__init__.py
+++ hasts/__init__.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
 
-### IMPORTS ###
+# This is a namespace placeholder.  Do not modify it and do not add anything to this file.
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-
-### GLOBALS ###
-
-### FUNCTIONS ###
-
-### CLASSES ###
